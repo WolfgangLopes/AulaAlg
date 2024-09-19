71bf3d84ce07bb0f6995c8ff2aa572fc859bf6a2
WolfgangLopes <wfglopes@gmail.com>
2024-09-19 19:48:16 -0300

    Add files via upload

---
+++ Algoritmo2.java
@@ -0,0 +1,15 @@
+package wlweb;
+import java.util.Scanner;
+
+public class Algoritmo2 {
+public static void main (String [] args){
+
+    Scanner read = new Scanner(System.in);
+    Double x,y;
+    System.out.println("Digite o valor de x");
+    x = read.nextDouble();
+    y = 3 + 2*x;
+    System.out.println(y);
+
+}
+}
---
+++ Pisos.java
@@ -0,0 +1,20 @@
+package wlweb;
+import java.util.Scanner
+
+public class Pisos {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        Double largura, comprimento, area;
+        int caixa;
+
+        System.out.println("Digite o comprimento");
+        comprimento = read.nextDouble();
+        System.out.println("Digite a largura");
+        largura = read.nextDouble();
+
+
+
+
+    }
+    
+}
